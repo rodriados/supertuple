c3d2e6ba4c3d6cb65214de8b8e618c6f774a9d24
Rodrigo Siqueira <rodriados@gmail.com>
2024-04-23 04:20:57 -0400

    [environment] Environment & version headers inclusion

---
+++ src/supertuple/environment.h
@@ -0,0 +1,61 @@
+/**
+ * SuperTuple: A powerful and light-weight C++ tuple implementation.
+ * @file Environment configuration and macro values
+ * @author Rodrigo Siqueira <rodriados@gmail.com>
+ * @copyright 2024-present Rodrigo Siqueira
+ */
+#pragma once
+
+#include <supertuple/version.h>
+
+#include <supertuple/environment/compiler.h>
+#include <supertuple/environment/language.h>
+#include <supertuple/environment/annotations.h>
+#include <supertuple/environment/namespace.hpp>
+
+/*
+ * Enumerates all possible target environment modes to which the code might be compiled
+ * to. The environment mode may affect some features' availability and performace.
+ */
+#define SUPERTUPLE_BUILD_DEV        0
+#define SUPERTUPLE_BUILD_DEBUG      1
+#define SUPERTUPLE_BUILD_TESTING    2
+#define SUPERTUPLE_BUILD_PRODUCTION 3
+
+/*
+ * Discovers and explicits the target environment mode to which the code must be
+ * currently compiled to. The mode may affect some features' availability and performance.
+ */
+#if defined(DEBUG) || defined(_DEBUG)
+  #define SUPERTUPLE_BUILD SUPERTUPLE_BUILD_DEBUG
+  #define SUPERTUPLE_ENVIRONMENT "Debug"
+#elif defined(TESTING)
+  #define SUPERTUPLE_BUILD SUPERTUPLE_BUILD_TESTING
+  #define SUPERTUPLE_ENVIRONMENT "Testing"
+#elif defined(PRODUCTION)
+  #define SUPERTUPLE_BUILD SUPERTUPLE_BUILD_PRODUCTION
+  #define SUPERTUPLE_ENVIRONMENT "Production"
+#else
+  #define SUPERTUPLE_BUILD SUPERTUPLE_BUILD_DEV
+  #define SUPERTUPLE_ENVIRONMENT "Development"
+#endif
+
+/*
+ * Checks the current compiler's C++ language level. As the majority of this software's
+ * codebase is written in C++, we must check whether its available or not.
+ */
+#if !defined(SUPERTUPLE_IGNORE_CPP_DIALECT)
+  #if !defined(SUPERTUPLE_CPP_DIALECT) || SUPERTUPLE_CPP_DIALECT < 2017
+    #error This software requires at least a C++17 enabled compiler.
+  #endif
+#endif
+
+/*
+ * Checks whether the current compiler is compatible with the software's prerequisites.
+ * Should it not be compatible, then we emit a warning but try compiling anyway.
+ */
+#if !defined(SUPERTUPLE_IGNORE_COMPILER_CHECK) && SUPERTUPLE_BUILD == SUPERTUPLE_BUILD_PRODUCTION
+  #if SUPERTUPLE_HOST_COMPILER == SUPERTUPLE_HOST_COMPILER_UNKNOWN
+    #warning This software has not been tested with the current compiler.
+  #endif
+#endif
---
+++ src/supertuple/version.h
@@ -0,0 +1,22 @@
+/**
+ * SuperTuple: A powerful and light-weight C++ tuple implementation.
+ * @file Compiler-time macros encoding SuperTuple release version.
+ * @author Rodrigo Siqueira <rodriados@gmail.com>
+ * @copyright 2024-present Rodrigo Siqueira
+ */
+#pragma once
+
+/**
+ * The preprocessor macros encoding the current Museqa library release version.
+ * This is guaranteed to change with every Museqa release.
+ */
+#define SUPERTUPLE_VERSION 10000
+
+/**
+ * The preprocessor macros encoding the release policy's values to the current SuperTuple
+ * library release version.
+ */
+#define SUPERTUPLE_VERSION_MAJOR (SUPERTUPLE_VERSION / 10000)
+#define SUPERTUPLE_VERSION_MINOR (SUPERTUPLE_VERSION / 100 % 100)
+#define SUPERTUPLE_VERSION_PATCH (SUPERTUPLE_VERSION % 100)
+/**#@-*/
