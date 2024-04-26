f79b4bf3fa14f65ed139c2955b1027daa4c07f07
Rodrigo Siqueira <rodriados@gmail.com>
2024-04-26 03:42:47 -0400

    [operation] Implementation of zipping operations

---
+++ src/supertuple/operation/zip.hpp
@@ -0,0 +1,64 @@
+/**
+ * SuperTuple: A powerful and light-weight C++ tuple implementation.
+ * @file The tuple zip operation implementation.
+ * @author Rodrigo Siqueira <rodriados@gmail.com>
+ * @copyright 2024-present Rodrigo Siqueira
+ */
+#pragma once
+
+#include <utility>
+
+#include <supertuple/environment.h>
+#include <supertuple/tuple.hpp>
+
+#include <supertuple/detail/utility.hpp>
+#include <supertuple/operation/get.hpp>
+
+SUPERTUPLE_BEGIN_NAMESPACE
+
+inline namespace operation
+{
+    /**
+     * Zips two tuples together eventually creating a tuple of pairs with types
+     * intercalated from the two original tuples.
+     * @tparam I The tuples' sequence indeces.
+     * @tparam T The first tuple's element members types.
+     * @tparam U The second tuple's element members types.
+     * @param a The first tuple to zip.
+     * @param b The second tuple to zip.
+     * @return The resulting zipped tuple.
+     */
+    template <size_t ...I, typename ...T, typename ...U>
+    SUPERTUPLE_CUDA_ENABLED inline constexpr decltype(auto) zip(
+        const tuple_t<detail::identity_t<std::index_sequence<I...>>, T...>& a
+      , const tuple_t<detail::identity_t<std::index_sequence<I...>>, U...>& b
+    ) {
+        return tuple_t(pair_t<T, U>(
+            operation::get<I>(a)
+          , operation::get<I>(b)
+        )...);
+    }
+
+    /**
+     * Zips two moving tuples together eventually creating a tuple of pairs with
+     * types intercalated from the two original tuples.
+     * @tparam I The tuples' sequence indeces.
+     * @tparam T The first tuple's element members types.
+     * @tparam U The second tuple's element members types.
+     * @param a The first tuple to zip.
+     * @param b The second tuple to zip.
+     * @return The resulting zipped tuple.
+     */
+    template <size_t ...I, typename ...T, typename ...U>
+    SUPERTUPLE_CUDA_ENABLED inline constexpr decltype(auto) zip(
+        tuple_t<detail::identity_t<std::index_sequence<I...>>, T...>&& a
+      , tuple_t<detail::identity_t<std::index_sequence<I...>>, U...>&& b
+    ) {
+        return tuple_t(pair_t<T, U>(
+            operation::get<I>(std::forward<decltype(a)>(a))
+          , operation::get<I>(std::forward<decltype(b)>(b))
+        )...);
+    }
+}
+
+SUPERTUPLE_END_NAMESPACE
---
+++ src/supertuple/operation/zipwith.hpp
@@ -0,0 +1,74 @@
+/**
+ * SuperTuple: A powerful and light-weight C++ tuple implementation.
+ * @file The tuple zipwith operation implementation.
+ * @author Rodrigo Siqueira <rodriados@gmail.com>
+ * @copyright 2024-present Rodrigo Siqueira
+ */
+#pragma once
+
+#include <utility>
+
+#include <supertuple/environment.h>
+#include <supertuple/tuple.hpp>
+
+#include <supertuple/detail/utility.hpp>
+#include <supertuple/operation/get.hpp>
+
+SUPERTUPLE_BEGIN_NAMESPACE
+
+inline namespace operation
+{
+    /**
+     * Zips two tuples together by combining paired elements with a given functor.
+     * Therefore, the resulting tuple does not contain pairs, but the result of
+     * each consecutive operation.
+     * @tparam F The functor type to combine the values with.
+     * @tparam I The tuples' sequence indeces.
+     * @tparam T The first tuple's element members types.
+     * @tparam U The second tuple's element members types.
+     * @param a The first tuple to zip.
+     * @param b The second tuple to zip.
+     * @param lambda The functor used to combine the elements.
+     * @return The resulting tuple.
+     */
+    template <typename F, size_t ...I, typename ...T, typename ...U>
+    SUPERTUPLE_CUDA_ENABLED inline constexpr decltype(auto) zipwith(
+        const tuple_t<detail::identity_t<std::index_sequence<I...>>, T...>& a
+      , const tuple_t<detail::identity_t<std::index_sequence<I...>>, U...>& b
+      , F&& lambda
+    ) {
+        return tuple_t(detail::invoke(
+            lambda
+          , operation::get<I>(a)
+          , operation::get<I>(b)
+        )...);
+    }
+
+    /**
+     * Zips two moving tuples together by combining paired elements with a given
+     * functor. Therefore, the resulting tuple does not contain pairs, but the result
+     * of each consecutive operation.
+     * @tparam F The functor type to combine the values with.
+     * @tparam I The tuples' sequence indeces.
+     * @tparam T The first tuple's element members types.
+     * @tparam U The second tuple's element members types.
+     * @param a The first tuple to zip.
+     * @param b The second tuple to zip.
+     * @param lambda The functor used to combine the elements.
+     * @return The resulting tuple.
+     */
+    template <typename F, size_t ...I, typename ...T, typename ...U>
+    SUPERTUPLE_CUDA_ENABLED inline constexpr decltype(auto) zipwith(
+        tuple_t<detail::identity_t<std::index_sequence<I...>>, T...>&& a
+      , tuple_t<detail::identity_t<std::index_sequence<I...>>, U...>&& b
+      , F&& lambda
+    ) {
+        return tuple_t(detail::invoke(
+            lambda
+          , operation::get<I>(std::forward<decltype(a)>(a))
+          , operation::get<I>(std::forward<decltype(b)>(b))
+        )...);
+    }
+}
+
+SUPERTUPLE_END_NAMESPACE
