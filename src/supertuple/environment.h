b7a6effce3a8df0c0aa13f8acc814c460485f82a
Rodrigo Siqueira <rodriados@gmail.com>
2024-04-28 03:55:29 -0400

    [environment] Unifying environment settings to a single file

--- src/supertuple/environment.h
+++ src/supertuple/environment.h
@@ -8,11 +8,6 @@
 
 #include <supertuple/version.h>
 
-#include <supertuple/environment/compiler.h>
-#include <supertuple/environment/language.h>
-#include <supertuple/environment/annotations.h>
-#include <supertuple/environment/namespace.hpp>
-
 /*
  * Enumerates all possible target environment modes to which the code might be compiled
  * to. The environment mode may affect some features' availability and performace.
@@ -41,6 +36,157 @@
 #endif
 
 /*
+ * Enumerating known host compilers. These compilers are not all necessarily officially
+ * supported. Nevertheless, some special adaptation or fixes might be implemented
+ * to each one of these if so needed.
+ */
+#define SUPERTUPLE_HOST_COMPILER_UNKNOWN 0
+#define SUPERTUPLE_HOST_COMPILER_GCC     1
+#define SUPERTUPLE_HOST_COMPILER_CLANG   2
+
+/*
+ * Enumerating known device compilers. These compilers are not all necessarily officially
+ * supported. Nevertheless, some special adaptation or fixes might be implemented
+ * to each one of these if so needed.
+ */
+#define SUPERTUPLE_DEVICE_COMPILER_UNKNOWN 0
+#define SUPERTUPLE_DEVICE_COMPILER_GCC     1
+#define SUPERTUPLE_DEVICE_COMPILER_CLANG   2
+#define SUPERTUPLE_DEVICE_COMPILER_NVCC    3
+
+/*
+ * Finds the version of the host compiler being used. Some features might change
+ * or be unavailable depending on the current compiler configuration.
+ */
+#if defined(__clang__)
+  #define SUPERTUPLE_HOST_COMPILER SUPERTUPLE_HOST_COMPILER_CLANG
+  #define SUPERTUPLE_CLANG_VERSION (__clang_major__ * 100 + __clang_minor__)
+#elif defined(__GNUC__) || defined(__GNUG__)
+  #define SUPERTUPLE_HOST_COMPILER SUPERTUPLE_HOST_COMPILER_GCC
+  #define SUPERTUPLE_GCC_VERSION (__GNUC__ * 100 + __GNUC_MINOR__)
+#else
+  #define SUPERTUPLE_HOST_COMPILER SUPERTUPLE_HOST_COMPILER_UNKNOWN
+#endif
+
+/*
+ * Finds the version of the device compiler being used. Some features might change
+ * or be unavailable depending on the current compiler configuration.
+ */
+#if defined(__clang__)
+  #define SUPERTUPLE_DEVICE_COMPILER SUPERTUPLE_DEVICE_COMPILER_CLANG
+#elif defined(__CUDACC__) || defined(__NVCOMPILER_CUDA__)
+  #define SUPERTUPLE_DEVICE_COMPILER SUPERTUPLE_DEVICE_COMPILER_NVCC
+#elif SUPERTUPLE_HOST_COMPILER == SUPERTUPLE_HOST_COMPILER_GCC
+  #define SUPERTUPLE_DEVICE_COMPILER SUPERTUPLE_DEVICE_COMPILER_GCC
+#else
+  #define SUPERTUPLE_DEVICE_COMPILER SUPERTUPLE_DEVICE_COMPILER_UNKNOWN
+#endif
+
+/*
+ * Macro for programmatically emitting a pragma call, independently on which compiler
+ * is currently in use.
+ */
+#if !defined(SUPERTUPLE_EMIT_PRAGMA_CALL)
+  #define SUPERTUPLE_EMIT_PRAGMA_CALL(x) _Pragma(#x)
+#endif
+
+/*
+ * Macro for programmatically emitting a compiler warning. This will be shown during
+ * compile-time and might stop compilation if warnings are treated as errors.
+ */
+#if !defined(SUPERTUPLE_EMIT_COMPILER_WARNING)
+  #if (SUPERTUPLE_HOST_COMPILER == SUPERTUPLE_HOST_COMPILER_GCC)
+    #define SUPERTUPLE_EMIT_COMPILER_WARNING(msg)           \
+      SUPERTUPLE_EMIT_PRAGMA_CALL(GCC warning msg)
+  #elif (SUPERTUPLE_HOST_COMPILER == SUPERTUPLE_HOST_COMPILER_CLANG)
+    #define SUPERTUPLE_EMIT_COMPILER_WARNING(msg)           \
+      SUPERTUPLE_EMIT_PRAGMA_CALL(clang warning msg)
+  #else
+    #define SUPERTUPLE_EMIT_COMPILER_WARNING(msg)
+  #endif
+#endif
+
+/*
+ * Macro for disabling or manually emitting warnings when compiling host code with
+ * GCC. This is useful for hiding buggy compiler warnings or compiler exploits that
+ * have intentionally been taken advantage of.
+ */
+#if (SUPERTUPLE_HOST_COMPILER == SUPERTUPLE_HOST_COMPILER_GCC) && !defined(__CUDA_ARCH__)
+  #define SUPERTUPLE_EMIT_GCC_WARNING(x) SUPERTUPLE_EMIT_COMPILER_WARNING(x)
+  #define SUPERTUPLE_DISABLE_GCC_WARNING_BEGIN(x)           \
+    SUPERTUPLE_EMIT_PRAGMA_CALL(GCC diagnostic push)        \
+    SUPERTUPLE_EMIT_PRAGMA_CALL(GCC diagnostic ignored x)
+  #define SUPERTUPLE_DISABLE_GCC_WARNING_END(x)             \
+    SUPERTUPLE_EMIT_PRAGMA_CALL(GCC diagnostic pop)
+#else
+  #define SUPERTUPLE_DISABLE_GCC_WARNING_BEGIN(x)
+  #define SUPERTUPLE_DISABLE_GCC_WARNING_END(x)
+  #define SUPERTUPLE_EMIT_GCC_WARNING(x)
+#endif
+
+#if (SUPERTUPLE_HOST_COMPILER == SUPERTUPLE_HOST_COMPILER_CLANG)
+  #define SUPERTUPLE_EMIT_CLANG_WARNING(x) SUPERTUPLE_EMIT_COMPILER_WARNING(x)
+  #define SUPERTUPLE_DISABLE_CLANG_WARNING_BEGIN(x)           \
+    SUPERTUPLE_EMIT_PRAGMA_CALL(clang diagnostic push)        \
+    SUPERTUPLE_EMIT_PRAGMA_CALL(clang diagnostic ignored x)
+  #define SUPERTUPLE_DISABLE_CLANG_WARNING_END(x)             \
+    SUPERTUPLE_EMIT_PRAGMA_CALL(clang diagnostic pop)
+#else
+  #define SUPERTUPLE_DISABLE_CLANG_WARNING_BEGIN
+  #define SUPERTUPLE_DISABLE_CLANG_WARNING_END
+  #define SUPERTUPLE_EMIT_CLANG_WARNING
+#endif
+
+/*
+ * Macro for pushing compiling options for GCC. These allow certain compiler behaviors
+ * to be modified for the code within the block of changed settings.
+ */
+#if (SUPERTUPLE_HOST_COMPILER == SUPERTUPLE_HOST_COMPILER_GCC) && !defined(__CUDA_ARCH__)
+  #define SUPERTUPLE_PUSH_GCC_OPTION_BEGIN(x)               \
+    SUPERTUPLE_EMIT_PRAGMA_CALL(GCC push_options)           \
+    SUPERTUPLE_EMIT_PRAGMA_CALL(GCC x)
+  #define SUPERTUPLE_PUSH_GCC_OPTION_END(x)                 \
+    SUPERTUPLE_EMIT_PRAGMA_CALL(GCC pop_options)
+#else
+  #define SUPERTUPLE_PUSH_GCC_OPTION_BEGIN(x)
+  #define SUPERTUPLE_PUSH_GCC_OPTION_END(x)
+#endif
+
+/*
+ * Macro for disabling warnings when compiling device code with NVCC. This is useful
+ * for hiding buggy compiler warnings or intentional compiler exploits.
+ */
+#if (SUPERTUPLE_DEVICE_COMPILER == SUPERTUPLE_DEVICE_COMPILER_NVCC)
+  #define SUPERTUPLE_DISABLE_NVCC_WARNING_BEGIN(x)          \
+    SUPERTUPLE_EMIT_PRAGMA_CALL(push)                       \
+    SUPERTUPLE_EMIT_PRAGMA_CALL(nv_diag_suppress = x)
+  #define SUPERTUPLE_DISABLE_NVCC_WARNING_END(x)            \
+    SUPERTUPLE_EMIT_PRAGMA_CALL(pop)
+#else
+  #define SUPERTUPLE_DISABLE_NVCC_WARNING_BEGIN(x)
+  #define SUPERTUPLE_DISABLE_NVCC_WARNING_END(x)
+#endif
+
+/*
+ * Discovers the C++ language dialect in use for the current compilation. A specific
+ * dialect might not be supported or might be required for certain functionalities
+ * to work properly.
+ */
+#if defined(__cplusplus)
+  #if __cplusplus < 201103L
+    #define SUPERTUPLE_CPP_DIALECT 2003
+  #elif __cplusplus < 201402L
+    #define SUPERTUPLE_CPP_DIALECT 2011
+  #elif __cplusplus < 201703L
+    #define SUPERTUPLE_CPP_DIALECT 2014
+  #elif __cplusplus == 201703L
+    #define SUPERTUPLE_CPP_DIALECT 2017
+  #elif __cplusplus > 201703L
+    #define SUPERTUPLE_CPP_DIALECT 2020
+  #endif
+#endif
+
+/*
  * Checks the current compiler's C++ language level. As the majority of this software's
  * codebase is written in C++, we must check whether its available or not.
  */
@@ -59,3 +205,53 @@
     #warning This software has not been tested with the current compiler.
   #endif
 #endif
+
+/*
+ * Since only NVCC knows how to deal with `__host__` and `__device__` annotations,
+ * we define them to empty strings when another compiler is in use. This allows
+ * the use of these annotations without needing to care whether they'll be known
+ * by the compiler or not.
+ */
+#if SUPERTUPLE_DEVICE_COMPILER == SUPERTUPLE_DEVICE_COMPILER_NVCC
+  #define SUPERTUPLE_CUDA_ENABLED __host__ __device__
+#else
+  #define SUPERTUPLE_CUDA_ENABLED
+#endif
+
+/*
+ * Macros for applying annotations and qualifiers to functions and methods. As the
+ * minimum required language version is C++17, we assume it is guaranteed that all
+ * compilers will have `inline` and `constexpr` implemented.
+ */
+#define SUPERTUPLE_INLINE SUPERTUPLE_CUDA_ENABLED inline
+#define SUPERTUPLE_CONSTEXPR SUPERTUPLE_INLINE constexpr
+
+/**
+ * Defines the namespace in which the library lives. This might be overriden if
+ * the default namespace value is already in use.
+ * @since 1.0
+ */
+#if defined(SUPERTUPLE_OVERRIDE_NAMESPACE)
+  #define SUPERTUPLE_NAMESPACE SUPERTUPLE_OVERRIDE_NAMESPACE
+#else
+  #define SUPERTUPLE_NAMESPACE supertuple
+#endif
+
+/**
+ * This macro is used to open the `supertuple::` namespace block and must not be
+ * in any way overriden. This namespace must not be prefixed by any other namespaces
+ * to avoid problems when allowing the use some of the library's facilities to with
+ * STL's algorithms, structures or constructions.
+ * @since 1.0
+ */
+#define SUPERTUPLE_BEGIN_NAMESPACE      \
+    namespace SUPERTUPLE_NAMESPACE {    \
+        inline namespace v1 {           \
+        namespace supertuple = ::SUPERTUPLE_NAMESPACE;
+
+/**
+ * This macro is used to close the `supertuple::` namespace block and must not be
+ * in any way overriden.
+ * @since 1.0
+ */
+#define SUPERTUPLE_END_NAMESPACE   }}
