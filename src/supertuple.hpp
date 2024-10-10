0f30cf71c3ff9c86d967657d886111ff3fb4f7e5
Rodrigo Siqueira <rodriados@gmail.com>
2024-10-10 01:03:33 -0400

    [api] Providing API entry point file

--- src/supertuple.hpp
+++ src/supertuple.hpp
@@ -1,32 +1,9 @@
 /**
  * SuperTuple: A powerful and light-weight C++ tuple implementation.
- * @file The project's main header file.
+ * @file The project's entry header file.
  * @author Rodrigo Siqueira <rodriados@gmail.com>
  * @copyright 2024-present Rodrigo Siqueira
  */
 #pragma once
 
-#include <supertuple/version.h>
-#include <supertuple/environment.h>
-
-#include <supertuple/tuple.hpp>
-
-#include <supertuple/operation/get.hpp>
-#include <supertuple/operation/set.hpp>
-#include <supertuple/operation/tie.hpp>
-#include <supertuple/operation/head.hpp>
-#include <supertuple/operation/last.hpp>
-#include <supertuple/operation/init.hpp>
-#include <supertuple/operation/tail.hpp>
-#include <supertuple/operation/reverse.hpp>
-#include <supertuple/operation/append.hpp>
-#include <supertuple/operation/prepend.hpp>
-#include <supertuple/operation/concat.hpp>
-#include <supertuple/operation/apply.hpp>
-#include <supertuple/operation/forward.hpp>
-#include <supertuple/operation/foreach.hpp>
-#include <supertuple/operation/fold.hpp>
-#include <supertuple/operation/scan.hpp>
-#include <supertuple/operation/zip.hpp>
-#include <supertuple/operation/zipwith.hpp>
-#include <supertuple/operation/convert.hpp>
+#include <supertuple/api.h>
---
+++ src/supertuple/api.h
@@ -0,0 +1,43 @@
+/**
+ * SuperTuple: A powerful and light-weight C++ tuple implementation.
+ * @file The project's API exposition header file.
+ * @author Rodrigo Siqueira <rodriados@gmail.com>
+ * @copyright 2024-present Rodrigo Siqueira
+ */
+#pragma once
+
+#ifndef SUPERTUPLE_HEADER_INCLUDED
+#define SUPERTUPLE_HEADER_INCLUDED
+
+#include <supertuple/version.h>
+#include <supertuple/environment.h>
+
+#include <supertuple/tuple.hpp>
+
+#include <supertuple/operation/get.hpp>
+#include <supertuple/operation/set.hpp>
+#include <supertuple/operation/tie.hpp>
+
+#include <supertuple/operation/head.hpp>
+#include <supertuple/operation/last.hpp>
+#include <supertuple/operation/init.hpp>
+#include <supertuple/operation/tail.hpp>
+
+#include <supertuple/operation/append.hpp>
+#include <supertuple/operation/prepend.hpp>
+#include <supertuple/operation/concat.hpp>
+#include <supertuple/operation/reverse.hpp>
+
+#include <supertuple/operation/fold.hpp>
+#include <supertuple/operation/scan.hpp>
+
+#include <supertuple/operation/apply.hpp>
+#include <supertuple/operation/foreach.hpp>
+#include <supertuple/operation/forward.hpp>
+
+#include <supertuple/operation/zip.hpp>
+#include <supertuple/operation/zipwith.hpp>
+
+#include <supertuple/operation/convert.hpp>
+
+#endif
