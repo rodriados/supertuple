00c4fac21f5a1fb61b3357376d64d8ef9e467b50
Rodrigo Siqueira <rodriados@gmail.com>
2025-01-18 03:37:37 -0400

    [cmake] Fixing catch2 find script

--- cmake/FindCatch2.cmake
+++ cmake/FindCatch2.cmake
@@ -2,27 +2,25 @@
 # @file Script responsible for finding the Catch2 project.
 # @author Rodrigo Siqueira <rodriados@gmail.com>
 # @copyright 2025-present Rodrigo Siqueira
+cmake_minimum_required(VERSION 3.24)
+
 include(FetchContent)
 
 set(CATCH2_REPOSITORY "https://github.com/catchorg/Catch2.git")
-set(CATCH2_REPOSITORY_TAG "v3.5.4")
+set(CATCH2_REPOSITORY_TAG "v3.9.1")
 
-# Let's try checking whether the project is already known and/or cached. If so,
-# we should avoid unnecessary downloads, thus improving build performance.
-find_package(Catch2 QUIET CONFIG)
+# Declares the remote source of the required package and allows it to be found.
+# If needed, the package will be downloaded and cached for build.
+FetchContent_Declare(
+  Catch2
+    GIT_SHALLOW true
+    GIT_REPOSITORY ${CATCH2_REPOSITORY}
+    GIT_TAG ${CATCH2_REPOSITORY_TAG}
+    OVERRIDE_FIND_PACKAGE)
 
-if(NOT Catch2_FOUND)
-  FetchContent_Declare(
-    Catch2
-      GIT_SHALLOW true
-      GIT_REPOSITORY ${CATCH2_REPOSITORY}
-      GIT_TAG ${CATCH2_REPOSITORY_TAG})
-  FetchContent_MakeAvailable(Catch2)
-endif()
-
-# Now that the package is guaranteed to be known, we must configure it so that its
-# variables and targets are made available in the callee context.
-find_package(Catch2 CONFIG REQUIRED)
+# Now that the package is declared, we must find and configure it so that its variables
+# and targets are made available for the parent context.
+find_package(Catch2 REQUIRED)
 
 # Also, a helper function for automatically finding tests is provided. We must include
 # this function as well, to provide a better downstream experience.
